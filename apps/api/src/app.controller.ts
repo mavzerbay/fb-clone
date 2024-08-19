b542c462020777de7e6de12771514da1f837a93b
Erbay MAVZER <mavzerbay@gmail.com>
2024-08-19 17:25:15 +0300

    chore: Add initial project configuration files and code structure

---
+++ apps/api/src/app.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Inject } from '@nestjs/common';
+import { AppService } from './app.service';
+import { ClientProxy } from '@nestjs/microservices';
+
+@Controller()
+export class AppController {
+  constructor(
+    @Inject('AUTH_SERVICE') private readonly authService: ClientProxy,
+  ) {}
+
+  @Get()
+  async getUser() {
+    return this.authService.send(
+      {
+        cmd: 'get-user',
+      },
+      {},
+    );
+  }
+}
